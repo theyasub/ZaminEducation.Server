583870eafc77b1db1ed77eb0ac197e946258df08
muham <muhammmadibnabdurashid@gmail.com>
2022-09-22 02:21:05 +0300

    DATA: IRepository.cs interface has been written.

---
+++ src/ZaminEducation.Data/IRepositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ZaminEducation.Data.IRepositories;
+
+public interface IRepository<TSource> where TSource : class
+{
+    IQueryable<TSource> GetAll(Expression<Func<TSource, bool>> expression = null, string include = null, bool isTracking = true);
+    Task<TSource> AddAsync(TSource entity);
+    Task<TSource> GetAsync(Expression<Func<TSource, bool>> expression = null, string include = null);
+    TSource Update(TSource entity);
+    void Delete(Expression<Func<TSource, bool>> expression);
+}
