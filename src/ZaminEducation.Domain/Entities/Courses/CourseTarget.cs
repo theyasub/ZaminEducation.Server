24ba0fec58fdbae7137b2e8499f56c256c4a58bf
Dastanbayev02 <dastanbayev02s@gmail.com>
2022-09-22 12:00:50 +0500

    CODE RUB: Remove Unaccessary Namespaces

--- src/ZaminEducation.Domain/Entities/Courses/CourseTarget.cs
+++ src/ZaminEducation.Domain/Entities/Courses/CourseTarget.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using ZaminEducation.Domain.Commons;
 
 namespace ZaminEducation.Domain.Entities.Courses
@@ -9,7 +6,6 @@ namespace ZaminEducation.Domain.Entities.Courses
     public class CourseTarget : Auditable
     {
         public string Name { get; set; }
-
         public long CourseId { get; set; }
         public Course Course { get; set; }
     }
