ab4c3397ef197a7d4cbc6aa8b7e669be03e4b72d
Jamshid <wonderboy1w3@gmail.com>
2022-09-24 06:31:14 +0500

    Implement extension methods and exception

---
+++ src/ZaminEducation.Domain/Configurations/PaginationMetaData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaminEducation.Domain.Configurations
+{
+    public class PaginationMetaData
+    {
+        public PaginationMetaData(int totalCount, PaginationParmas @parmas)
+        {
+            TotalCount = totalCount;
+            TotalPage = (int) Math.Ceiling(totalCount / (double) parmas.PageSize);
+            CurrentPage = parmas.PageIndex;
+        }
+        public int CurrentPage { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPage { get; set; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalCount;
+    }
+}
---
+++ src/ZaminEducation.Service/Extensions/StringExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaminEducation.Service.Extensions
+{
+    public static class StringExtension
+    {
+        public static string Encrypt(this string password)
+        {
+            using var md5 = MD5.Create();
+            var data = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            return Encoding.UTF8.GetString(data);
+        }
+    }
+}
