a9c1f4fb1fda7422b838b36eb9688f7932a7fd18
MuhammadaminNet <MburanbayevDev@gmail.com>
2022-09-24 15:27:25 +0500

    Data: Task Changed to ValueTask

--- src/ZaminEducation.Data/IRepositories/IRepository.cs
+++ src/ZaminEducation.Data/IRepositories/IRepository.cs
@@ -8,10 +8,10 @@ namespace ZaminEducation.Data.IRepositories
     public interface IRepository<TSource> where TSource : class
     {
         IQueryable<TSource> GetAll(Expression<Func<TSource, bool>> expression = null, string[] includes = null, bool isTracking = true);
-        Task<TSource> AddAsync(TSource entity);
-        Task<TSource> GetAsync(Expression<Func<TSource, bool>> expression, string[] includes);
+        ValueTask<TSource> AddAsync(TSource entity);
+        ValueTask<TSource> GetAsync(Expression<Func<TSource, bool>> expression, string[] includes);
         TSource Update(TSource entity);
         void Delete(TSource entity);
-        Task SaveChangesAsync();
+        ValueTask SaveChangesAsync();
     }
 }
