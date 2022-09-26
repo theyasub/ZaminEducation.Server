e532742b8efc003b8d0ded3bc4aadb134bf9ed34
Ozodbek00 <kamoliddinovozodbek@mail.ru>
2022-09-27 00:46:49 +0500

    YoutubeVideo model migrated | YouTubeService complete

--- src/ZaminEducation.Data/Migrations/20220926194440_youtubeModelMigration.Designer.cs
+++ src/ZaminEducation.Data/Migrations/20220926194440_youtubeModelMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZaminEducation.Data.DbContexts;
 
@@ -11,9 +12,10 @@ using ZaminEducation.Data.DbContexts;
 namespace ZaminEducation.Data.Migrations
 {
     [DbContext(typeof(ZaminEducationDbContext))]
-    partial class ZaminEducationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220926194440_youtubeModelMigration")]
+    partial class youtubeModelMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
