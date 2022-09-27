f3b4171636e1267f87271c05283b181ed63c24d0
Muhammad <muhammadmamushah.com@gmail.com>
2022-09-27 18:10:32 +0500

    Api: Added file extension and size validation

---
+++ src/ZaminEducation.Api/Extensions/Attributes/FormFileExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ZaminEducation.Api.Extensions.Attributes
+{
+    public static class FormFileExtensions
+    {
+        public static bool IsValid(this IFormFile file)
+        {
+            string[] extensions = new string[] { ".png", ".jpg" };
+            var extension = Path.GetExtension(file.FileName);
+
+            return extensions.Contains(extension.ToLower());
+        }
+
+        public static bool NoMoreThanRequiredFileSize(this IFormFile file) =>
+             file.Length <= 1048576;
+    }
+}
---
+++ src/ZaminEducation.Service/Interfaces/IQuizService.cs
@@ -0,0 +1,10 @@
+﻿using ZaminEducation.Domain.Entities.Quizzes;
+using ZaminEducation.Service.DTOs.Quizzes;
+
+namespace ZaminEducation.Service.Interfaces
+{
+    public interface IQuizService
+    {
+        ValueTask<Quiz> CreateAsync(QuizForCreationDto quizForCreationDto);
+    }
+}
