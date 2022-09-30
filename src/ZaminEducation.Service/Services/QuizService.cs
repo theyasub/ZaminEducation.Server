d6dfb09343befd470c79a2c5a2d163610d7996f5
MuhammadaminNet <MburanbayevDev@gmail.com>
2022-09-30 10:58:03 +0500

    Quiz service bug fixed

--- src/ZaminEducation.Service/Services/QuizService.cs
+++ src/ZaminEducation.Service/Services/QuizService.cs
@@ -2,7 +2,10 @@
 using ZaminEducation.Data.IRepositories;
 using ZaminEducation.Domain.Entities.Quizzes;
 using ZaminEducation.Service.DTOs.Quizzes;
+using ZaminEducation.Service.Exceptions;
+using ZaminEducation.Service.Extensions;
 using ZaminEducation.Service.Interfaces;
+using ZaminEducation.Service.Interfaces.Courses;
 
 namespace ZaminEducation.Service.Services
 {
@@ -13,11 +16,15 @@ namespace ZaminEducation.Service.Services
         private readonly IRepository<QuizAsset> quizAssetRepository;
         private readonly IRepository<QuestionAnswer> answerRepository;
         private readonly IMapper mapper;
+        private readonly ICourseService courseService;
+        private readonly IAttachmentService attachmentService;
 
         public QuizService(IRepository<Quiz> quizRepository,
             IRepository<QuizContent> quizContentRepository,
             IRepository<QuizAsset> quizAssetRepository,
             IRepository<QuestionAnswer> answerRepository,
+            ICourseService courseService,
+            IAttachmentService attachmentService,
             IMapper mapper)
         {
             this.quizRepository = quizRepository;
@@ -25,31 +32,67 @@ namespace ZaminEducation.Service.Services
             this.quizAssetRepository = quizAssetRepository;
             this.answerRepository = answerRepository;
             this.mapper = mapper;
+            this.courseService = courseService;
+            this.attachmentService = attachmentService;
         }
 
-        public async ValueTask<Quiz> CreateAsync(long courseId, QuizForCreationDto quizForCreationDto, QuizContentForCreationDto questionDto)
+        public async ValueTask<Quiz> CreateAsync(long courseId,
+            QuizForCreationDto quizDto, QuizContentForCreationDto questionDto)
         {
-            await quizContentRepository.AddAsync(
-                mapper.Map<QuizContent>(questionDto));
+            var course = await courseService.GetAsync(c => c.Id == courseId);
 
-            var quiz = await quizRepository.AddAsync(
-                mapper.Map<Quiz>(quizForCreationDto));
+            if (course is null)
+                throw new ZaminEducationException(400,"Bad request");
 
+            var content = mapper.Map<QuizContent>(questionDto);
+            content.Create();
+
+            content = await quizContentRepository.AddAsync(content);
+
+            var quiz = mapper.Map<Quiz>(quizDto);
+            quiz.QuestionId = content.Id;
+            quiz.CourseId = courseId;
+            quiz.Create();
+
+            await quizRepository.AddAsync(quiz);
             await quizRepository.SaveChangesAsync();
 
             return quiz;
         }
 
-        public async ValueTask<QuestionAnswer> CreateAnswerAsync(QuestionAnswerForCreationDto answerForCreationDto)
+        public async ValueTask<QuestionAnswer> CreateAnswerAsync(
+            QuestionAnswerForCreationDto dto)
         {
-            var answer = await answerRepository.AddAsync(mapper.Map<QuestionAnswer>(answerForCreationDto));
+            var content = await quizContentRepository.GetAsync(c => c.Id == dto.ContentId);
+            var quiz = await quizRepository.GetAsync(q => q.Id == dto.QuizId);
+            var answer = await answerRepository.GetAsync(a => a.Id == dto.QuestionId);
+
+            if (answer is null || quiz is null || content is null)
+                throw new ZaminEducationException(400, "Bad request");
+
+            answer = mapper.Map<QuestionAnswer>(dto);
+            answer.Create(); 
+
+            await answerRepository.AddAsync(answer);
+            await answerRepository.SaveChangesAsync();
 
             return answer;
         }
 
-        public async ValueTask<QuizAsset> CreateAssetsAsync(QuizAssetForCreationDto assetForCreationDto)
+        public async ValueTask<QuizAsset> CreateAssetsAsync(
+            QuizAssetForCreationDto dto)
         {
-            var asset = await quizAssetRepository.AddAsync(mapper.Map<QuizAsset>(assetForCreationDto));
+            var content = await quizContentRepository.GetAsync(c => c.Id == dto.ContentId);
+            var attachent = await attachmentService.GetAsync(a => a.Id == dto.FileId);
+
+            if(content is null && attachent is null)
+                throw new ZaminEducationException(400,"Bad request");
+
+            var asset = mapper.Map<QuizAsset>(dto);
+            asset.Create();
+
+            asset = await quizAssetRepository.AddAsync(asset);
+            await quizAssetRepository.SaveChangesAsync();
 
             return asset;
         }
