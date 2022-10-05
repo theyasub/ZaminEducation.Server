4966fa14f5c4e8c69722cd6842f0b9686886258d
Temurkhann <109779514+Temurkhann@users.noreply.github.com>
2022-10-06 01:02:41 +0500

    CODE RUB: Bug Fixing

--- src/ZaminEducation.Api/Controllers/UsersController.cs
+++ src/ZaminEducation.Api/Controllers/UsersController.cs
@@ -22,9 +22,9 @@ public class UsersController : BaseController
     }
 
     /// <summary>
-    /// Create new user
+    /// create new user
     /// </summary>
-    /// <param name="dto">user creating initial info taker dto</param>
+    /// <param name="dto">user create</param>
     /// <returns>Created user infortaions</returns>
     /// <response code="200">If user is created successfully</response>
     [HttpPost]
@@ -33,7 +33,7 @@ public class UsersController : BaseController
         Ok(await userService.CreateAsync(dto));
 
     /// <summary>
-    /// delete user by id (for only admins)
+    /// delete user
     /// </summary>
     /// <param name="id"></param>
     /// <returns>true if user deleted succesfully else false</returns>
@@ -43,7 +43,7 @@ public class UsersController : BaseController
 
 
     /// <summary>
-    /// get all of users (for only admins)
+    /// get all of users
     /// </summary>
     /// <param name="params">pagenation params</param>
     /// <returns> user collection </returns>
@@ -51,19 +51,24 @@ public class UsersController : BaseController
     public async ValueTask<ActionResult<IEnumerable<User>>> GetAllAsync(
         [FromQuery] PaginationParams @params) =>
             Ok(await userService.GetAllAsync(@params));
-
-    [HttpPost("Change/Password"), Authorize(Policy = "AllPolicy")]
+    
+    /// <summary>
+    /// update password
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    [HttpPost("password"), Authorize("AllPolicy")]
     public async ValueTask<ActionResult<User>> ChangePasswordAsync(UserForChangePassword dto) =>
         Ok(await userService.ChangePasswordAsync(dto));
 
     /// <summary>
-    /// get one user information by id
+    /// get one user information
     /// </summary>
     /// <param name="id">user id</param>
     /// <returns>user</returns>
     /// <response code="400">if user data is not in the base</response>
     /// <response code="200">if user data have in database</response>
-    [HttpGet("{id}"), Authorize(Roles  = "AllPolicy")]
+    [HttpGet("{id}"), Authorize("AllPolicy")]
     public async ValueTask<ActionResult<User>> GetAsync([FromRoute]long id) =>
         Ok(await userService.GetAsync(user => user.Id == id));
 
@@ -75,11 +80,11 @@ public class UsersController : BaseController
     /// <returns></returns>
     [HttpPut, Authorize("AllPolicy")]
     public async ValueTask<ActionResult<User>> UpdateAsync(
-        long id, [FromBody] UserForCreationDto dto) => 
+        long id, [FromBody] UserForUpdateDto dto) => 
             Ok(await userService.UpdateAsync(id, dto));
 
     /// <summary>
-    /// get self user info without any id
+    /// get self user info
     /// </summary>
     /// <returns>user</returns>
     [HttpGet("info"), Authorize]
@@ -87,10 +92,10 @@ public class UsersController : BaseController
         => Ok(await userService.GetInfoAsync());
 
     /// <summary>
-    /// create attachment for user for all id
+    /// create attachment
     /// </summary>
     /// <returns></returns>
-    [HttpPost("attachments/{id}"), Authorize(Roles = "UserPolicy")]
+    [HttpPost("attachments/{id}"), Authorize("UserPolicy")]
     public async Task<IActionResult> Attachment(long id, IFormFile formFile)
         => Ok(await userService.AddAttachmentAsync(id, formFile.ToAttachmentOrDefault()));
 }
