6d88675058d32ae2c0ca74cf4425ff1d06e5ce88
Muhammad <muhammadmamushah.com@gmail.com>
2022-10-06 10:13:23 +0500

    Test: Unit Tests Changed To Service Test For User Created New Memory Database For Test

---
+++ Tests/ZaminEducation.Service.Unit/Services/UserServiceTest.Get.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Force.DeepCloner;
+using System.Threading.Tasks;
+using ZaminEducation.Domain.Entities.Users;
+using ZaminEducation.Service.DTOs.Users;
+
+namespace ZaminEducation.Test.Unit.Services
+{
+    public partial class UserServiceTest
+    {
+        [Fact]
+        public async Task ShouldGetUserById()
+        {
+            // given
+            UserForCreationDto randomUser = CreateRandomUser(new UserForCreationDto());
+            UserForCreationDto inputUser = randomUser;
+            UserForCreationDto expectedUser = inputUser.DeepClone();
+
+            // when
+            User actualUser = await userService.CreateAsync(inputUser);
+            User gotUser = await userService.GetAsync(u => u.Id == actualUser.Id);
+
+            // then
+            actualUser.Should().NotBeNull();
+            gotUser.Should().NotBeNull();
+            actualUser.Should().BeEquivalentTo(gotUser);
+        }
+
+
+    }
+}
---
+++ Tests/ZaminEducation.Service.Unit/Services/UserServiceTest.Modify.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Force.DeepCloner;
+using System.Threading.Tasks;
+using ZaminEducation.Domain.Entities.Users;
+using ZaminEducation.Service.DTOs.Users;
+
+namespace ZaminEducation.Test.Unit.Services
+{
+    public partial class UserServiceTest
+    {
+        [Fact]
+        public async Task ShouldUpdateUser()
+        {
+            UserForCreationDto randomUser = CreateRandomUser(new UserForCreationDto());
+            UserForCreationDto inputUser = randomUser;
+            UserForCreationDto expectedUser = inputUser.DeepClone();
+            UserForCreationDto inputUserForUpdate = inputUser.DeepClone();
+            inputUserForUpdate.LastName = Faker.Name.Last();
+            // when
+            User actualUser = await userService.CreateAsync(inputUser);
+
+            User actualUpdatedUser = await userService.UpdateAsync(actualUser.Id, inputUserForUpdate);
+
+            // then
+            actualUser.Should().NotBeNull();
+            actualUpdatedUser.Should().NotBeNull();
+
+            actualUser.Username.Should().BeEquivalentTo(expectedUser.Username);
+            actualUpdatedUser.Username.Should().BeEquivalentTo(inputUserForUpdate.Username);
+        }
+    }
+}
