bac841c619e42121aa3700a4803f13e979546d37
Dastanbayev02 <dastanbayev02s@gmail.com>
2022-10-15 15:03:12 +0500

    CODE RUB: Some Errors Have Been Fixed

--- src/ZaminEducation.Service/Services/YouTubeService.cs
+++ src/ZaminEducation.Service/Services/YouTubeService.cs
@@ -54,12 +54,7 @@ namespace ZaminEducation.Service.Services
         {
             IEnumerable<string> links = await GetLinksAsync(youtubePlaylist);
 
-            IEnumerable<string> oldLinks = await this.youtubeRepository.GetAll(cv => cv.CourseId.Equals(courseId)).Select(cv => cv.Url).ToListAsync();
-
             ICollection<CourseVideo> videos = new List<CourseVideo>();
-            
-            if (oldLinks.SequenceEqual<string>(links))
-                return await this.youtubeRepository.GetAll(cv => cv.CourseId.Equals(courseId)).ToListAsync();
 
             var yt = new YoutubeClient();
 
